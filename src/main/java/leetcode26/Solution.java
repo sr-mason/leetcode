509947b123980bd94041b1c5d993d72928575a5a
mason <mason2802@gmail.com>
2021-07-13 15:05:42 +0300

    leetcode26

---
+++ src/main/java/leetcode26/Solution.java
@@ -0,0 +1,18 @@
+package leetcode26;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return 1;
+        int j = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]){
+                nums[j++] = nums[i];
+            }
+        }
+        if (nums[j] != nums[nums.length - 1]) {
+            nums[j] = nums[nums.length - 1];
+        }
+        return ++j;
+    }
+}
