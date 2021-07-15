7f7b4431b9c40d1dfba54ac39eae898e6a7459b2
mason <mason2802@gmail.com>
2021-07-15 13:54:47 +0300

    leetcode977

---
+++ src/main/java/leetcode977/Solution.java
@@ -0,0 +1,18 @@
+package leetcode977;
+
+public class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int [] result = new int[nums.length];
+        int count = result.length - 1;
+        while (count >= 0) {
+            if (Math.abs(nums[start]) > Math.abs(nums[end])) {
+                result[count--] = (int) Math.pow(nums[start++], 2);
+            } else {
+                result[count--] = (int) Math.pow(nums[end--], 2);
+            }
+        }
+        return result;
+    }
+}
