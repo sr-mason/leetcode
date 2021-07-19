1189cb29f60dab3da4b280653d4b280d090c7400
mason <mason2802@gmail.com>
2021-07-19 09:47:06 +0300

    leetcode125

---
+++ src/main/java/leetcode125/Solution.java
@@ -0,0 +1,13 @@
+package leetcode125;
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        String temp = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        for (int i = 0; i < temp.length(); i++) {
+            if (temp.charAt(i) != temp.charAt(temp.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
