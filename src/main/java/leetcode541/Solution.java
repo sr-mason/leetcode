80d90a1f2a1d038b736b35e2056d651450ff389c
mason <mason2802@gmail.com>
2021-07-26 18:57:51 +0300

    leetcode541

---
+++ src/main/java/leetcode541/Solution.java
@@ -0,0 +1,30 @@
+package leetcode541;
+
+public class Solution {
+    public String reverseStr(String s, int k) {
+        if (s.length() == 1) {
+            return s;
+        }
+        if (k > s.length()) {
+            k = s.length();
+        }
+        StringBuilder temp = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        int flag = 0;
+        int step =  k;
+        while (i < s.length()) {
+            temp = (flag % 2 == 0) ? temp.append(s, i, step).reverse()
+                    : temp.append(s, i, step);
+            result.append(temp);
+            temp.delete(0,temp.length());
+            flag++;
+            i += k;
+            step += k;
+            if (step > s.length()) {
+                step = s.length();
+            }
+        }
+        return result.toString();
+    }
+}
