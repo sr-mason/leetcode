6bcd92f7ad6f8b573f4af91184fa75c673fa74e9
mason <mason2802@gmail.com>
2021-07-31 11:01:51 +0300

    leetcode151

---
+++ src/main/java/leetcode151/Solution.java
@@ -0,0 +1,16 @@
+package leetcode151;
+
+public class Solution {
+    public String reverseWords(String s) {
+        String[] array = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            if (array[array.length - i - 1] != null
+                    && array[array.length - i - 1].length() != 0) {
+                sb.append(array[array.length - i - 1]).append(" ");
+            }
+         }
+        sb.delete(sb.length() - 1, sb.length());
+        return sb.toString();
+    }
+}
