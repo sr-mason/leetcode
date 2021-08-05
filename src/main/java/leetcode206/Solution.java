8d03b2350da7b12c35bd1af842d811d49a1b4f8f
mason <mason2802@gmail.com>
2021-08-05 16:32:51 +0300

    leetcode206

---
+++ src/main/java/leetcode206/Solution.java
@@ -0,0 +1,23 @@
+package leetcode206;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+       ListNode prev = null;
+       ListNode current = head;
+       while (current != null) {
+           ListNode nxt = current.next;
+           current.next = prev;
+           prev = current;
+           current = nxt;
+       }
+       return prev;
+    }
+}
